51c09b8e5bc6d7ce8400897f0dc0ee4c2fe6fcec
aegistudio <1910567646@qq.com>
2017-02-19 11:24:02 +0800

    Add color.

--- demo/snake.cpp
+++ demo/snake.cpp
@@ -20,7 +20,20 @@ using namespace cng;
 	x = z >>     16;\
 	y = z  & 0xffff;
 
-DefaultMainLoop mainLoop;
+class SnakeLoop : public DefaultMainLoop {
+public:
+    const short outline = 1;
+    const short snake = 2;
+    const short food = 3;
+
+	virtual void init() {
+		DefaultMainLoop::init();
+		init_pair(outline, COLOR_CYAN, COLOR_BLACK);
+		init_pair(snake, COLOR_GREEN, COLOR_BLACK);
+		init_pair(food, COLOR_YELLOW, COLOR_BLACK);
+	}
+
+}mainLoop;
 
 class SnakeGame : public Handler {
 	std::deque<int> location;
@@ -52,21 +65,26 @@ public:
 
 	void paintGame() {
 		standout();
+		attron(COLOR_PAIR(mainLoop.snake));
 		std::deque<int>::iterator iter;
 		for(iter = location.begin(); iter != location.end(); iter ++) {
 			int posx, posy;
 			FROM_SNAKEPAIR(*iter, posx, posy);
 			mvprintw(posy + 1, posx + 1, " ");
 		}
+		attroff(COLOR_PAIR(mainLoop.snake));
 
+		attron(COLOR_PAIR(mainLoop.food));
 		int foodx, foody;
 		FROM_SNAKEPAIR(food, foodx, foody);
 		mvprintw(foody + 1, foodx + 1, "+");
 		standend();
+		attroff(COLOR_PAIR(mainLoop.food));
 	}
 
 	void paintBorder() {
 		standout();
+		attron(COLOR_PAIR(mainLoop.outline));
 		for(int i = 1; i <= boundx; i ++) {
 			mvprintw(0, i, "-");
 			mvprintw(boundy + 1, i, "-");
@@ -82,10 +100,14 @@ public:
 		mvprintw(boundy + 1, 0, "+");
 		mvprintw(boundy + 1, boundx + 1, "+");
 		mvprintw(0, boundx + 1, "-");
+		attroff(COLOR_PAIR(mainLoop.outline));
+		standend();
 
+		attron(COLOR_PAIR(mainLoop.outline));
 		char score[256];
 		sprintf(score, " Score: %d ", location.size() - initLength);
         mvprintw(0, (boundx - strlen(score))/ 2, score);
+		attroff(COLOR_PAIR(mainLoop.outline));
 	}
 
 	virtual void sync() override {
