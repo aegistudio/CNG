bdd388dec05f702288af2a5fad4d5d3cfb834c9c
aegistudio <1910567646@qq.com>
2017-02-19 14:47:13 +0800

    Two chars per snake block, and can pause now.

--- demo/snake.cpp
+++ demo/snake.cpp
@@ -17,14 +17,14 @@ using namespace cng;
 #define TO_SNAKEPAIR(z, x, y)\
 	z = x << 16 | y;
 #define FROM_SNAKEPAIR(z, x, y)\
-	x = z >>     16;\
+	x = z >>	 16;\
 	y = z  & 0xffff;
 
 class SnakeLoop : public DefaultMainLoop {
 public:
-    const short outline = 1;
-    const short snake = 2;
-    const short food = 3;
+	const short outline = 1;
+	const short snake = 2;
+	const short food = 3;
 
 	virtual void init() {
 		DefaultMainLoop::init();
@@ -36,6 +36,7 @@ public:
 }mainLoop;
 
 class SnakeGame : public Handler {
+	bool paused = false;
 	std::deque<int> location;
 	int dirx = 1, diry = 0;
 	int food;	int boundx, boundy;
@@ -54,9 +55,9 @@ public:
 	}
 
 	void newFood() {
-        int newx = rand() % boundx;
-        int newy = rand() % boundy;
-        TO_SNAKEPAIR(food, newx, newy);
+		int newx = rand() % boundx;
+		int newy = rand() % boundy;
+		TO_SNAKEPAIR(food, newx, newy);
 	}
 
 	void gameover() {
@@ -70,14 +71,14 @@ public:
 		for(iter = location.begin(); iter != location.end(); iter ++) {
 			int posx, posy;
 			FROM_SNAKEPAIR(*iter, posx, posy);
-			mvprintw(posy + 1, posx + 1, " ");
+			mvprintw(posy + 1, 2 * posx + 1, "  ");
 		}
 		attroff(COLOR_PAIR(mainLoop.snake));
 
 		attron(COLOR_PAIR(mainLoop.food));
 		int foodx, foody;
 		FROM_SNAKEPAIR(food, foodx, foody);
-		mvprintw(foody + 1, foodx + 1, "+");
+		mvprintw(foody + 1, 2 * foodx + 1, "  ");
 		standend();
 		attroff(COLOR_PAIR(mainLoop.food));
 	}
@@ -85,55 +86,74 @@ public:
 	void paintBorder() {
 		standout();
 		attron(COLOR_PAIR(mainLoop.outline));
-		for(int i = 1; i <= boundx; i ++) {
+		for(int i = 1; i <= 2 * boundx; i ++) {
 			mvprintw(0, i, "-");
 			mvprintw(boundy + 1, i, "-");
 		}
 
 		for(int i = 1; i <= boundy + 1; i ++) {
 			mvprintw(i, 0, "|");
-			mvprintw(i, boundx + 1, "|");
+			mvprintw(i, 2 * boundx + 1, "|");
 		}
 
 
 		mvprintw(0, 0, "+");
 		mvprintw(boundy + 1, 0, "+");
-		mvprintw(boundy + 1, boundx + 1, "+");
-		mvprintw(0, boundx + 1, "-");
+		mvprintw(boundy + 1, 2 * boundx + 1, "+");
+		mvprintw(0, 2 * boundx + 1, "-");
 		attroff(COLOR_PAIR(mainLoop.outline));
 		standend();
 
 		attron(COLOR_PAIR(mainLoop.outline));
 		char score[256];
 		sprintf(score, " Score: %d ", location.size() - initLength);
-        mvprintw(0, (boundx - strlen(score))/ 2, score);
+		mvprintw(0, boundx - strlen(score)/ 2, score);
 		attroff(COLOR_PAIR(mainLoop.outline));
 	}
 
 	virtual void sync() override {
+		// Draw paused in the middle of screen.
+		if(paused) {
+			const char* pause0 = "+-----------+";
+			const char* pause1 = "|   PAUSE   |";
+			const char* pause2 = "+-----------+";
+			int lenPause = strlen(pause1);
+			int pausePosY = boundy / 2;
+			int pausePosX = 1 + boundx - lenPause / 2;
+
+			standout();
+			attron(COLOR_PAIR(mainLoop.outline));
+			mvprintw(pausePosY - 1, pausePosX, pause0);
+			mvprintw(pausePosY + 0, pausePosX, pause1);
+			mvprintw(pausePosY + 1, pausePosX, pause2);
+			attroff(COLOR_PAIR(mainLoop.outline));
+			standend();
+			return;
+		}
+
 		// Move snake.
 		int headPosX, headPosY;
 		FROM_SNAKEPAIR(location.front(), headPosX, headPosY);
 
-        headPosX += dirx;
-        headPosY += diry;
+		headPosX += dirx;
+		headPosY += diry;
 
-        int newPosition;
-        TO_SNAKEPAIR(newPosition, headPosX, headPosY);
+		int newPosition;
+		TO_SNAKEPAIR(newPosition, headPosX, headPosY);
 
 		if(headPosX < 0 || headPosX >= boundx) { gameover(); return; }
 		if(headPosY < 0 || headPosY >= boundy) { gameover(); return; }
 
 		std::deque<int>::iterator iter;
 		for(iter = location.begin(); iter != location.end(); iter ++)
-            if(*iter == newPosition) { gameover(); return; }
+			if(*iter == newPosition) { gameover(); return; }
 
-        // Add new body portion.
-        if(newPosition == food)
+		// Add new body portion.
+		if(newPosition == food)
 			newFood();
-        else location.pop_back();
+		else location.pop_back();
 
-        location.push_front(newPosition);
+		location.push_front(newPosition);
 
 		wclear(stdscr);
 		paintGame();
@@ -176,6 +196,10 @@ public:
 					diry =  0;
 				}
 			break;
+
+			case ' ':
+				paused = !paused;
+			break;
 		}
 	}
 };
@@ -183,7 +207,7 @@ public:
 #pragma pop(scroll)
 
 int main() {
-	SnakeGame game(64, 16);
+	SnakeGame game(32, 16);
 	mainLoop.enter(&game);
 	mainLoop.loopMain();
 	return 0;
